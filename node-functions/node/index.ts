9c01fc0afee77016a3739c3e0c950ec742c9fa17
Nattsu39 <nattsu39@outlook.com>
2025-11-13 21:37:57 +0800

    测试响应头

--- node-functions/node/index.ts
+++ node-functions/node/index.ts
@@ -2,5 +2,10 @@ export function onRequestGet(context) {
 
   return new Response(null, {
     status: 304,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Cache-Control': 'no-cache'
+    },
   });
 }
