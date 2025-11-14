faba9fe3997fedd565cc00ff173aa06138585b26
linktang <tomleader4444@gmail.com>
2025-11-14 20:10:54 +0800

    Refactor onRequestGet to improve response handling
    Refactor onRequestGet function to use Response methods directly and clean up commented code.

--- node-functions/node/index.ts
+++ node-functions/node/index.ts
@@ -5,31 +5,34 @@ export function getEtagFromRequest(request): string {
   return request.headers.get('if-none-match') || request.headers.get('If-None-Match') || '';
 }
 
-export async function onRequestGet(context) {
-  return new Response(null, {
-    status: 304
-  });
-}
-
 // export async function onRequestGet(context) {
-//   const etag = getEtagFromRequest(context.request);
-//   if (etag) {
-//     return new Response(null, {
-//       status: 304,
-//       headers: {
-//         'Content-Type': 'application/json',
-//         'Access-Control-Allow-Origin': '*',
-//         'Cache-Control': 'no-cache',
-//       }
-//     });
-//   }
-//   return new Response(JSON.stringify({ test: 'ok' }), {
-//     status: 200,
-//     headers: {
-//       'Content-Type': 'application/json',
-//       'Access-Control-Allow-Origin': '*',
-//       'Cache-Control': 'no-cache',
-//       'ETag': '1234567890',
-//     }
+//   return new Response(null, {
+//     status: 304
 //   });
 // }
+
+export async function onRequestGet(context) {
+  const etag = getEtagFromRequest(context.request);
+  if (etag) {
+    const res = new Response();
+    res.setStatus(304)
+    return res;
+    // return new Response(null, {
+    //   status: 304,
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //     'Access-Control-Allow-Origin': '*',
+    //     'Cache-Control': 'no-cache',
+    //   }
+    // });
+  }
+  return new Response(JSON.stringify({ test: 'ok' }), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Cache-Control': 'no-cache',
+      'ETag': '1234567890',
+    }
+  });
+}
