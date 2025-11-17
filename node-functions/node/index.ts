b913000ea7f90c3154d785a8281b2b3e6c8d4624
linktang <tomleader4444@gmail.com>
2025-11-17 10:39:13 +0800

    Clean up commented code in index.ts
    Removed commented-out code for onRequestGet function.

--- node-functions/node/index.ts
+++ node-functions/node/index.ts
@@ -5,12 +5,6 @@ export function getEtagFromRequest(request): string {
   return request.headers.get('if-none-match') || request.headers.get('If-None-Match') || '';
 }
 
-// export async function onRequestGet(context) {
-//   return new Response(null, {
-//     status: 304
-//   });
-// }
-
 export async function onRequestGet(context) {
   const etag = getEtagFromRequest(context.request);
   if (etag) {
